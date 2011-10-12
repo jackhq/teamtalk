7b3ce4885faad6073cf51e81b76b52450c8dda72
Tom Wilson <tom@jackhq.com>
2011-10-12 00:06:36 -0400

    added basic auth

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
   nowjs = require('now');
   sugar = require('sugar');
   messages = require('./messages');
-  messages.init(process.env.MONGOHQ_URL || 'localhost:27017/teamtalk');
   app.configure(function() {
     app.set("views", __dirname + "/views");
     app.set("view engine", "jade");
@@ -33,6 +32,7 @@
   app.listen(process.env.PORT || 3000, function() {
     return console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
   });
+  messages.init(process.env.MONGOHQ_URL || 'localhost:27017/teamtalk');
   everyone = nowjs.initialize(app);
   everyone.now.distribute = function(msg) {
     messages.add({
